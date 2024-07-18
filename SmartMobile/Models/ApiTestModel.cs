b84d248077d5a03409a28baa4017455d83464baa
Gabriel Vinicius Ramos de Oliveira <gabriel14fev@gmail.com>
2024-07-18 08:59:24 -0300

    first

---
+++ SmartMobile/Models/ApiTestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMobile.Models
+{
+    public class ApiTestModel
+    {
+        public string Acao { get; set; }
+        public string? DataHora { get; set; }
+        public string? Nome { get; set; }
+    }
+
+}
---
+++ SmartMobile/Models/Register/RegisterDataGrid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMobile.Models.Register
+{
+    public class RegisterDataGrid
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Document { get; set; }
+        public string Status { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
